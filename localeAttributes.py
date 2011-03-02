d5574bec16803c8177bed9bab395ce359f3a86cd
Daniel Black <erectlocution@gmail.com>
2011-03-02 15:21:18 -0500

    Added some filler locale data.

---
+++ localeAttributes.py
@@ -0,0 +1,27 @@
+# Specify the following information for each locale:
+
+# x:
+# y:
+# desc:
+# 
+# x:
+# y:
+# desc:
+# 
+# x:
+# y:
+# desc:
+# 
+# x:
+# y:
+# desc:
+# 
+# x:
+# y:
+# desc:
+# 
+# x:
+# y:
+# desc:
+# 
+
